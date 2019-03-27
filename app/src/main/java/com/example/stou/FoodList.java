70159429d15717392d2806973c0553b3d0018f1c
tkotekar <tejaswi.kotekar@gmail.com>
2019-03-26 23:55:13 -0400

    Fixed Bugs from first Sprint
    -Added foodDetail.java and activity_food_detail.xml
    -added money xml
    -changed styles.xml
    -changed Android Manifest.xml
    -App build gradle changes

--- app/src/main/java/com/example/stou/FoodList.java
+++ app/src/main/java/com/example/stou/FoodList.java
@@ -1,5 +1,6 @@
 package com.example.stou;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -63,7 +64,10 @@ public class FoodList extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(FoodList.this, ""+local.getName(),Toast.LENGTH_SHORT).show();
+                        Intent foodDetail = new Intent(FoodList.this, FoodDetail.class);
+                        foodDetail.putExtra("FoodId", adapter.getRef(position).getKey());
+                        startActivity(foodDetail);
+                        //Toast.makeText(FoodList.this, ""+local.getName(),Toast.LENGTH_SHORT).show();
                     }
                 });
             }
